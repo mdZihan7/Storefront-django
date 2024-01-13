cd0fbe4756be1426657084b2e43b4d7a30d166a3
Zihan-ma1610630 <zihan.efran@gmail.com>
2024-01-13 21:53:58 +0300

    .

--- playground/views.py
+++ playground/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+# request -> response
+# request handler
+# action 
+
+def say_hello(request) : 
+    # pull data from db
+    # Transform
+    # Send email
+    # return HttpResponse('Hello world')
+    return render(request, 'hello.html')
